463ccecb7306f0c470762f36f50947630986ffe7
Ahmed elbadri <aelbadri23@gmail.com>
2026-05-27 17:10:06 +0300

    feat: configure app branding and location permissions while adding new Zekr UI components

--- lib/feature/ui/screens/azkar/widgets/zekr_card.dart
+++ lib/feature/ui/screens/azkar/widgets/zekr_card.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:quran_azkar/core/helpers/padding.dart';
 import 'package:quran_azkar/core/model/azkar_model.dart';
+import 'package:quran_azkar/feature/ui/screens/azkar/widgets/zekr_card_widget.dart';
 import 'package:quran_azkar/generated/l10n.dart';
 
 import '../../../../logic/cubit/settings_cubit.dart';
@@ -88,100 +87,7 @@ class _ZekrCardState extends State<ZekrCard> {
               );
             },
             onTap: onTapDownCounter,
-            child: Card(
-              margin: const EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 8.0,
-              ),
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    if (widget.zekr.title != null &&
-                        widget.zekr.title!.isNotEmpty) ...[
-                      Text(
-                        widget.zekr.title!,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 18 * fontScale,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.teal,
-                        ),
-                      ),
-                      const Divider(height: 24),
-                    ],
-                    if (widget.zekr.content != null &&
-                        widget.zekr.content!.isNotEmpty) ...[
-                      Text(
-                        widget.zekr.content!,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 20 * fontScale,
-                          height: 1.5,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      verticalSpace(16.h),
-                    ],
-                    if (widget.zekr.note != null &&
-                        widget.zekr.note!.isNotEmpty) ...[
-                      Text(
-                        widget.zekr.note!,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 14 * fontScale,
-                          color: Colors.grey,
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                      verticalSpace(16.h),
-                    ],
-                    if (widget.zekr.referance != null &&
-                        widget.zekr.referance!.isNotEmpty) ...[
-                      Text(
-                        widget.zekr.referance!,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 14 * fontScale,
-                          color: Colors.blueGrey,
-                        ),
-                      ),
-                      verticalSpace(16.h),
-                    ],
-                    if (widget.zekr.repeat != null && widget.zekr.repeat! > 0)
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 16,
-                            vertical: 8,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.teal.withOpacity(0.1),
-                            borderRadius: BorderRadius.circular(16),
-                            border: Border.all(
-                              color: Colors.teal.withOpacity(0.5),
-                            ),
-                          ),
-                          child: Text(
-                            'التكرار: ${widget.downcounter}',
-                            style: TextStyle(
-                              fontSize: 14 * fontScale,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.teal,
-                            ),
-                          ),
-                        ),
-                      ),
-                  ],
-                ),
-              ),
-            ),
+            child: ZekrCardWidget(widget: widget, fontScale: fontScale),
           );
         },
       ),
---
+++ lib/feature/ui/screens/azkar/widgets/zekr_card_column.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:quran_azkar/feature/ui/screens/azkar/widgets/zekr_card.dart';
+
+import '../../../../../core/helpers/padding.dart';
+
+class ZekrCardColumn extends StatelessWidget {
+  const ZekrCardColumn({
+    super.key,
+    required this.widget,
+    required this.fontScale,
+  });
+
+  final ZekrCard widget;
+  final double fontScale;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        if (widget.zekr.title != null && widget.zekr.title!.isNotEmpty) ...[
+          Text(
+            widget.zekr.title!,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18 * fontScale,
+              fontWeight: FontWeight.bold,
+              color: Colors.teal,
+            ),
+          ),
+          const Divider(height: 24),
+        ],
+        if (widget.zekr.content != null && widget.zekr.content!.isNotEmpty) ...[
+          Text(
+            widget.zekr.content!,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20 * fontScale,
+              height: 1.5,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          verticalSpace(16.h),
+        ],
+        if (widget.zekr.note != null && widget.zekr.note!.isNotEmpty) ...[
+          Text(
+            widget.zekr.note!,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 14 * fontScale,
+              color: Colors.grey,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          verticalSpace(16.h),
+        ],
+        if (widget.zekr.referance != null &&
+            widget.zekr.referance!.isNotEmpty) ...[
+          Text(
+            widget.zekr.referance!,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 14 * fontScale, color: Colors.blueGrey),
+          ),
+          verticalSpace(16.h),
+        ],
+        if (widget.zekr.repeat != null && widget.zekr.repeat! > 0)
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              decoration: BoxDecoration(
+                color: Colors.teal.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(color: Colors.teal.withOpacity(0.5)),
+              ),
+              child: Text(
+                'التكرار: ${widget.downcounter}',
+                style: TextStyle(
+                  fontSize: 14 * fontScale,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal,
+                ),
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}
